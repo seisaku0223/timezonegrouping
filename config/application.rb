262a7863c19c231c9b283a74a47da163eb7519c2
seisaku0223 <kdoll135npc@outlook.jp>
2018-09-26 04:36:16 +0000

    change config default_locale = ja, time_zone = Tokyo, active_record.default_timezone = local, generators not create file assets and helper = false

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,14 @@ module Timezonegrouping
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
