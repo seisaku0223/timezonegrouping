b3f522bbcd6f2b29343a4b1ae164abb37510b61b
seisaku0223 <kdoll135npc@outlook.jp>
2018-10-03 07:50:13 +0000

    Add event list, event new registration

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,21 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new(params.require(:event).permit(:title, :content, :requirement))
+    @event.manager_id = current_user.id
+
+    if @event.save
+      redirect_to events_path, notice: "イベントを登録しました！"
+    else
+      render 'new'
+    end
   end
 
   def show
