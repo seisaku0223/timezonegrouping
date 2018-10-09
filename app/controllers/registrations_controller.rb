b335484efa538d2376a88a918f73917ce2b0cb62
seisaku0223 <kdoll135npc@outlook.jp>
2018-10-09 04:31:41 +0000

    Change, event registration function. Register and delete only

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,27 +1,32 @@
 class RegistrationsController < ApplicationController
   def create
-    # @registration = current_user.registrations.build(params.permit(:event_id))
-    @registration = current_user.registrations.build(registrations_params)
-    @event = @registration.event
+    @registration = current_user.registrations.build(registration_params)
+    set_event
 
     respond_to do |format|
       if @registration.save
-        format.html { redirect_to event_path(@event), notice: '参加登録しました' }
+        format.html { redirect_to event_path(@event), notice: "参加登録しました" }
+        format.js { render :index }
       end
     end
   end
 
   def destroy
-  end
-
-  def edit
-  end
-
-  def update
+    @registration = Registration.find(params[:id])
+    set_event
+    @registration.destroy
+    respond_to do |format|
+      format.html { redirect_to event_path(@event), notice: "参加登録をキャンセルしました" }
+      format.js { render :index }
+    end
   end
 
   private
-  def registrations_params
-    params.require(:registration).permit(:event_id, :desire_day, :desire_time, :on_requirement)
-  end
+    def registration_params
+      params.require(:registration).permit(:event_id, :start_datetime, :end_datetime, :on_requirement)
+    end
+
+    def set_event
+      @event = @registration.event
+    end
 end
